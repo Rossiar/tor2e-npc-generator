85026cac19fa36f5e9fbde09ad24db738aa51f76
Rossiar <rosshenderson92@gmail.com>
2022-02-28 23:20:20 +0000

    feat: add bardings paternal relation

--- pages/cultures/bardings.js
+++ pages/cultures/bardings.js
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import styles from '../../styles/Home.module.css'
 
 export default function Barding() {
+    var d = data[gender[Math.floor(Math.random() * 2)]]
     return (
         <main className={styles.main}>
             <h1 className={styles.title}>
-                {random(gender[Math.floor(Math.random() * 2)])}
+                {random(d["names"])}, {d["relation"]} of {random(data["male"]["names"])}
             </h1>
-            <h2>({features.sort(() => 0.5 - Math.random()).slice(0, 2).join(", ")})</h2>
+            <h2>({data["features"].sort(() => 0.5 - Math.random()).slice(0, 2).join(", ")})</h2>
             <Link href="/">
                 <a><h2>Back</h2></a>
             </Link>
@@ -19,24 +20,30 @@ function random(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-const males = [
-    "Aegir", "Arn", "Brandulf", "Domarr", "Egil", "Erland", "Farald",
-    "Finn", "Gautarr", "Hafgrim", "Hjalmar", "Ingolf", "Jofur",
-    "Kolbeinn", "Leiknir", "Lomund", "Munan", "Nari", "Nefstan",
-    "Ottarr", "Ragnarr", "Reinald", "Sigmarr", "Steinarr", "Thorald",
-    "Torwald", "Ulfarr", "Unnarr", "Vandil", "Varinn"
-]
+const data = {
+    "female": {
+        "relation": "daughter",
+        "names": [
+            "Aldis", "Asfrid", "Bera", "Bergdis", "Dagmar", "Eilif", "Erna",
+            "Frida", "Geira", "Gudrun", "Halla", "Hild", "Ingirun", "Ingrith",
+            "Lif", "Linhild", "Kelda", "Runa", "Saldis", "Sigga", "Sigrun",
+            "Thora", "Thordis", "Thorhild", "Ulfhild", "Ulfrun", "Una",
+            "Valdis", "Vigdis", "Walda"
+        ]
+    },
+    "male": {
+        "relation": "son",
+        "names": [
+            "Aegir", "Arn", "Brandulf", "Domarr", "Egil", "Erland", "Farald",
+            "Finn", "Gautarr", "Hafgrim", "Hjalmar", "Ingolf", "Jofur",
+            "Kolbeinn", "Leiknir", "Lomund", "Munan", "Nari", "Nefstan",
+            "Ottarr", "Ragnarr", "Reinald", "Sigmarr", "Steinarr", "Thorald",
+            "Torwald", "Ulfarr", "Unnarr", "Vandil", "Varinn"
+        ],
+    },
+    "features": [
+        "Bold", "Eager", "Fair", "Fierce", "Generous", "Proud", "Tall", "Wilful"    
+    ]
+}
 
-const females = [
-    "Aldis", "Asfrid", "Bera", "Bergdis", "Dagmar", "Eilif", "Erna",
-    "Frida", "Geira", "Gudrun", "Halla", "Hild", "Ingirun", "Ingrith",
-    "Lif", "Linhild", "Kelda", "Runa", "Saldis", "Sigga", "Sigrun",
-    "Thora", "Thordis", "Thorhild", "Ulfhild", "Ulfrun", "Una",
-    "Valdis", "Vigdis", "Walda"
-]
-
-const gender = [males, females]
-
-const features = [
-    "Bold", "Eager", "Fair", "Fierce", "Generous", "Proud", "Tall", "Wilful"
-]
+const gender = ["male", "female"]
