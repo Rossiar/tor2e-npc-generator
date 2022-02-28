f3606015877401b564e5e3126750eb6279da20ad
Rossiar <rosshenderson92@gmail.com>
2022-02-28 22:40:08 +0000

    feat: clear down template and make first culture page

---
+++ pages/cultures/bardings.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css'
+
+export default function Barding() {
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                {random(gender[Math.floor(Math.random() * 2)])} ({features.sort(() => 0.5 - Math.random()).slice(0, 2).join(", ")})
+            </h1>
+            <Link href="/">
+                <a><h2>Back</h2></a>
+            </Link>
+        </main>
+    )
+}
+
+function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+const males = [
+    "Aegir", "Arn", "Brandulf", "Domarr", "Egil", "Erland", "Farald",
+    "Finn", "Gautarr", "Hafgrim", "Hjalmar", "Ingolf", "Jofur",
+    "Kolbeinn", "Leiknir", "Lomund", "Munan", "Nari", "Nefstan",
+    "Ottarr", "Ragnarr", "Reinald", "Sigmarr", "Steinarr", "Thorald",
+    "Torwald", "Ulfarr", "Unnarr", "Vandil", "Varinn"
+]
+
+const females = [
+    "Aldis", "Asfrid", "Bera", "Bergdis", "Dagmar", "Eilif", "Erna",
+    "Frida", "Geira", "Gudrun", "Halla", "Hild", "Ingirun", "Ingrith",
+    "Lif", "Linhild", "Kelda", "Runa", "Saldis", "Sigga", "Sigrun",
+    "Thora", "Thordis", "Thorhild", "Ulfhild", "Ulfrun", "Una",
+    "Valdis", "Vigdis", "Walda"
+]
+
+const gender = [males, females]
+
+const features = [
+    "Bold", "Eager", "Fair", "Fierce", "Generous", "Proud", "Tall", "Wilful"
+]
